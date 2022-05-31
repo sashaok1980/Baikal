a1f044a87f5bc32dff8303542a53500026ad110a
sasha.ok <sasha.ok1980@mail.ru>
2022-05-31 13:44:38 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,7 @@
+$(function(){
+    $('.photo_slider').slick({
+        prevArrow:'<button type="button" class="slick-prev"><img src="Куба/left-1.png" alt=""></button>',
+        nextArrow:'<button type="button" class="slick-next"><img src="Куба/right-2.png" alt=""></button>'
+    });
+
+});
